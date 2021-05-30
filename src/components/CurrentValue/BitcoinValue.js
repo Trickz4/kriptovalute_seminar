ea0b50bfd7083565b90de8b4539cf2d648718483
Trickz4 <roko123.zd@gmail.com>
2021-05-30 18:52:52 +0200

    Setup basic UI (custom button and card)

---
+++ src/components/CurrentValue/BitcoinValue.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "../UI/Button";
+
+import Card from "../UI/Card";
+
+import classes from "./BitcoinValue.module.css";
+
+const BitcoinValue = (props) => {
+
+
+
+  const showCurrentPriceHandler = (event) => {
+    event.preventDefault();
+    console.log("pressing button works!");
+  };
+
+  return (
+    <Card customClassName={classes.bitcoin_current_price}>
+      <label id="ageLabel" htmlFor="age">
+        1 Bitcoin in $USD: {}
+      </label>
+      <p>Price placeholder</p>
+      <Button buttonType="submit" onButtonClick={showCurrentPriceHandler}>
+        Click to show current Bitcoin price
+      </Button>
+    </Card>
+  );
+};
+
+export default BitcoinValue;
