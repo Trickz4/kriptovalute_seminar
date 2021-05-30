275750a4fb871335a280eac8d7c92779e5be359a
Trickz4 <roko123.zd@gmail.com>
2021-05-30 18:52:07 +0200

    Setup initial react project and its dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddUser from './components/Users/AddUser';
+import BitcoinValue from './components/CurrentValue/BitcoinValue'
+
+
+function App() {
+  return (
+    <div>
+     {/*  <AddUser/> */}
+     <BitcoinValue></BitcoinValue>
+    </div>
+  );
+}
+
+export default App;
